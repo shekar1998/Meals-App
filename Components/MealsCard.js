cb5628978266011d5ac25ba7afcec81ea6f1fba6
your_username <email>
2021-11-29 20:36:46 +0530

    v3

--- Components/MealsCard.js
+++ Components/MealsCard.js
@@ -1,25 +1,36 @@
 import { setStatusBarBackgroundColor } from 'expo-status-bar';
 import { Box } from 'native-base';
 import React from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, Image } from 'react-native';
 import color from '../assets/color';
 
 const bgcolor = color.light;
 const MealsCard = (props) => {
   console.log('props => ', props.item.name);
   return (
-    <Box 
-    justifyContent={'center'} 
-    w={185} 
-    h={210} 
-    bg={bgcolor.text} 
-    borderRadius={10}
-    m={2}>
-      <Text style={{ color: '#fff' }}>{props.item.name}</Text>
+    <Box justifyContent={'space-between'} w={185} h={210} borderRadius={15} m={2}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{ uri: props.item.imageURL }} />
+      </View>
+      <Text style={{ color: '#000' }}>{props.item.name}</Text>
     </Box>
   );
 };
 
 export default MealsCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  imageContainer: {
+    width: '100%',
+    height: 150,
+    borderRadius: 15,
+    overflow: 'hidden',
+    borderWidth: 0.5,
+    elevation:1
+  },
+});
